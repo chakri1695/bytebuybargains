361a556927cd341440fcc4dd50c064e6f13a4e11
Chakri1695 <chakravarthiraju66@gmail.com>
2025-04-06 09:45:16 +0530

    Fix vite config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
- HEAD
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-});
-
-import { defineConfig } from 'vite'; import react from '@vitejs/plugin-react'; export default defineConfig({ plugins: [react()] });
- 19de41a (Initial project push)
+})
+s
