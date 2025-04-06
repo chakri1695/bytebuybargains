fd48db614ad1bd4b493fd9fe2dd349caa86c6b5a
Chakri1695 <chakravarthiraju66@gmail.com>
2025-04-06 09:07:38 +0530

    Initial project push

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
+ HEAD
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
 });
+
+import { defineConfig } from 'vite'; import react from '@vitejs/plugin-react'; export default defineConfig({ plugins: [react()] });
+ 19de41a (Initial project push)
